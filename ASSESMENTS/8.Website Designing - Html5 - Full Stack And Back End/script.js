2bb7eb44725d43afe25e6be6799d1a24ecdf2c24
kimu7466 <kimu7466@gmail.com>
2024-06-02 11:15:44 +0530

    #

---
+++ ASSESMENTS/8.Website Designing - Html5 - Full Stack And Back End/script.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     let f_name = document.getElementById("firstName")
+
+//     f_name.addEventListener("input",()=>{
+//         console.log(f_name.value);
+//     })
+
+// })
